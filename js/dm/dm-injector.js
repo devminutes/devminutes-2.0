1e39e99efedd697cdfae00b83e6ecf2482e60cfe
jirkapenzes <jirkapenzes@gmail.com>
2015-05-28 15:12:28 +0200

    Code refactoring

--- js/dm/dm-injector.js
+++ js/dm/dm-injector.js
@@ -1,45 +1,46 @@
 var EasterEgg = function (sequence) {
-    var self = this;
-    var position = 0;
+  var self = this;
+  var position = 0;
 
-    var hide = function () {
-      var egg = document.getElementById('easteregg');
-      if(egg) egg.remove();
+  var hide = function () {
+    var easteregg = document.getElementById('easteregg');
+    if(easteregg) easteregg.remove();
+  }
+
+  document.addEventListener('keypress', hide);
+  document.addEventListener('mousedown', hide);
+  document.addEventListener('scroll', hide);
+  document.addEventListener('keypress', function (event) {
+    if(event.keyCode == sequence.charCodeAt(position)) {
+      position += 1;
+
+      if(position == sequence.length) {
+        position = 0;
+        self.callback();
+      }
+    } else {
+      position = 0;
     }
-
-    document.addEventListener('keypress', hide);
-    document.addEventListener('mousedown', hide);
-    document.addEventListener('scroll', hide);
-    document.addEventListener('keypress', function (e) {
-       if(e.keyCode == sequence.charCodeAt(position)) {
-           position += 1;
-           if(position == sequence.length) {
-               position = 0;
-               self.callback();
-           }
-       } else {
-           position = 0;
-       }
-    });
+  });
 };
 
 EasterEgg.prototype.then = function(f) {
-    this.callback = f;
+  this.callback = f;
 };
 
 new EasterEgg('czpodcast').then(function () {
-  var dmHomer = document.createElement('div');
-  dmHomer.id = 'easteregg';
-  dmHomer.style.position = 'absolute';
-  dmHomer.style.width = '100%';
-  dmHomer.style.height = '100%';
-  dmHomer.style.backgroundColor = 'rgba(255,255,255,0.5)';
-  dmHomer.style.backgroundImage = 'url(../images/homer.jpg)';
-  dmHomer.style.backgroundRepeat = 'no-repeat';
-  dmHomer.style.backgroundPosition = 'center center';
-  dmHomer.style.backgroundSize = 'contain';
-  dmHomer.style.left = document.body.scrollLeft + 'px';
-  dmHomer.style.top = document.body.scrollTop + 'px';
-  dmHomer.style.zIndex = 9999;
-  document.body.insertBefore(dmHomer, document.body.firstChild);
+  var easteregg = document.createElement('div');
+  easteregg.id = 'easteregg';
+  easteregg.style.position = 'absolute';
+  easteregg.style.width = '100%';
+  easteregg.style.height = '100%';
+  easteregg.style.backgroundColor = 'rgba(255,255,255,0.5)';
+  easteregg.style.backgroundImage = 'url(../images/homer.jpg)';
+  easteregg.style.backgroundRepeat = 'no-repeat';
+  easteregg.style.backgroundPosition = 'center center';
+  easteregg.style.backgroundSize = 'contain';
+  easteregg.style.left = document.body.scrollLeft + 'px';
+  easteregg.style.top = document.body.scrollTop + 'px';
+  easteregg.style.zIndex = 9999;
+  document.body.insertBefore(easteregg, document.body.firstChild);
 }); 
